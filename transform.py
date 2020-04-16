e9c172edf08766b18e659f4a661625b9c42eee60
miltonjdaz <miltonjdaz@yahoo.com>
2020-04-16 14:30:00 -0500

    Keeping the username/password safe

--- transform.py
+++ transform.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
 
-df = pd.read_csv("/home/milton/github/Runescape/data/drops2.csv")
-df.info()
-df.head()
+db_user = os.environ.get('DB_USER')
+db_pw = os.environ.get('DB_PASS')
+
+# example dialect+driver://username:password@host:port/database
+
+engine = create_engine('postgresql+psycopg2://{login}:{pass}@localhost/postgres'.format(login=db_user, pass=db_pw))
 
