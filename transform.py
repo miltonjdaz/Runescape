93553689c3c341de3ed62af10e6b3086b856052e
miltonjdaz <miltonjdaz@yahoo.com>
2020-04-23 10:57:00 -0500

    Still busy w/finals

--- transform.py
+++ transform.py
@@ -10,16 +10,19 @@ db_pw = os.environ.get('DB_PASS')
 
 engine = create_engine('postgresql://{login}:{pw}@localhost/postgres'.format(login=db_user, pw=db_pw))
 
-query_one = pd.read_sql_query('SELECT COUNT(Dragon_bones) FROM rstable GROUP BY Day ORDER BY Day', engine)
+query_one = pd.read_sql_query('SELECT COUNT(Dragon_bones), Day FROM rstable GROUP BY Day ORDER BY Day', engine)
 print(query_one)
 
 query_two = pd.read_sql_query('SELECT COUNT(Rune_hasta), Day FROM rstable GROUP BY Day ORDER BY Day', engine)
 print(query_two)
 
-query_three = pd.read_sql_query('SELECT AVG(Coins), Day FROM rstable GROUP BY Day ORDER BY Day', engine)
+query_three = pd.read_sql_query('SELECT AVG(Rune_Hasta), Day FROM rstable GROUP BY Day ORDER BY Day', engine)
 print(query_three)
 
-query_four = pd.read_sql_query('SELECT COUNT(Clue_scroll_hard) FROM rstable GROUP BY Day ORDER BY Day', engine)
+query_four = pd.read_sql_query('SELECT COUNT(Clue_scroll_hard), Day FROM rstable GROUP BY Day ORDER BY Day', engine)
 print(query_four)
+
+query_five = pd.read_sql_query('SELECT AVG(Clue_scroll_hard), Day FROM rstable GROUP BY Day ORDER BY Day', engine)
+print(query_five)
 
 import ipdb; ipdb.set_trace()
