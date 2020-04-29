30f324f8e3a156dce6c84e5418ea5ab37fdc35af
miltonjdaz <miltonjdaz@yahoo.com>
2020-04-29 08:54:43 -0500

    added some comments

--- results/final_results.py
+++ results/final_results.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from matplotlib import pyplot as plt
- 
+
+# 1st graph from the 1st query 
+# Be sure to have the correct path in order to connect properly 
+# Since the x axis labels were overlapping, I had to put plt.xticks rotation = 30
+# As such, the dates for each of the days can be properly seen
+
 df1 = pd.read_csv("/home/milton/github/Runescape/results/results_q1.csv")
 
 x = df1.day
@@ -12,6 +17,8 @@ plt.xlabel("Day")
 plt.ylabel("Dragon_bones")
 plt.xticks(rotation=30)
 plt.show()
+
+# 2nd graph from the 2nd query 
 
 df2 = pd.read_csv("/home/milton/github/Runescape/results/results_q2.csv")
 
@@ -25,6 +32,8 @@ plt.ylabel("Rune_hasta")
 plt.xticks(rotation=30)
 plt.show()
 
+# 3rd graph from the 3rd query 
+
 df3 = pd.read_csv("/home/milton/github/Runescape/results/results_q3.csv")
 
 x = df3.day
@@ -36,6 +45,8 @@ plt.xlabel("Day")
 plt.ylabel("Blood_rune")
 plt.xticks(rotation=30)
 plt.show()
+
+# 4th graph from the 4th query 
 
 df4 = pd.read_csv("/home/milton/github/Runescape/results/results_q4.csv")
 
